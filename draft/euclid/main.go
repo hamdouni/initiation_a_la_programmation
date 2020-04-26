bb9dccc50f6b2dd5a747fed06164371c2254a56a
Brahim Hamdouni <brahim@hamdouni.com>
2020-04-26 21:11:43 +0200

    Corrige une faute d'orthographe

--- draft/euclid/main.go
+++ draft/euclid/main.go
@@ -1,7 +1,7 @@
 /*
 Plus Grand Diviseur Commun
 
-Rappel : le PGCD de deux nombres entiers n et m est le plus grang nombre possible, qui divise à la fois n et m.
+Rappel : le PGCD de deux nombres entiers n et m est le plus grand nombre possible, qui divise à la fois n et m.
 
 Euclid a proposé un algorithme pour résoudre ce problème comme suit :
 
